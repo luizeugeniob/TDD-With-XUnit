1320268c8fbde3c54904c37c63b6decd84feb479
Luiz Eugênio Barbieri <luizeugeniobarbieri@gmail.com>
2020-10-24 16:33:50 -0300

    Padronização da nomenclatura dos testes seguindo o conceito Given, When e Then.

---
+++ tests/EAuction.Core.Tests/AuctionReceiveBid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace EAuction.Core.Tests
+{
+    public class AuctionReceiveBid
+    {
+        [Fact]
+        public void DoesNotAllowReceiveNewBidWhenAuctionIsClosed()
+        {
+            //Arrange
+            var auction = new Auction("Van Gogh");
+            var johnDoe = new Interested("John Doe", auction);
+            auction.ReceiveBid(johnDoe, 800);
+            auction.ReceiveBid(johnDoe, 900);
+            auction.ClosesAuction();
+
+            //Act
+            auction.ReceiveBid(johnDoe, 1000);
+
+            //Assert
+            Assert.Equal(2, auction.Bids.Count());
+        }
+    }
+}
