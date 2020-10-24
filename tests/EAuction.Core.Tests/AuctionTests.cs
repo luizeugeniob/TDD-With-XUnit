3fbebcde0ac7a3ef479cd5cfac61596c6b08cfdc
Luiz Eugênio Barbieri <luizeugeniobarbieri@gmail.com>
2020-10-24 15:37:22 -0300

    Implementação de mais testes unitários.

--- tests/EAuction.Core.Tests/AuctionTests.cs
+++ tests/EAuction.Core.Tests/AuctionTests.cs
@@ -5,7 +5,7 @@ namespace EAuction.Core.Tests
     public class AuctionTests
     {
         [Fact]
-        public void Auction_With_Many_Of_Bids()
+        public void Auction_With_Many_Bids()
         {
             //Arrange
             var auction = new Auction("Van Gogh");
@@ -16,6 +16,26 @@ namespace EAuction.Core.Tests
             auction.ReceiveBid(janeDoe, 900);
             auction.ReceiveBid(johnDoe, 1000);
             auction.ReceiveBid(janeDoe, 990);
+
+            //Act
+            auction.ClosesAuction();
+
+            //Assert
+            Assert.Equal(1000, auction.Winner.Amount);
+        }
+
+        [Fact]
+        public void Auction_With_Many_Ordered_Bids()
+        {
+            //Arrange
+            var auction = new Auction("Van Gogh");
+            var johnDoe = new Interested("John Doe", auction);
+            var janeDoe = new Interested("Jane Doe", auction);
+
+            auction.ReceiveBid(johnDoe, 800);
+            auction.ReceiveBid(janeDoe, 900);
+            auction.ReceiveBid(janeDoe, 990);
+            auction.ReceiveBid(johnDoe, 1000);
 
             //Act
             auction.ClosesAuction();
@@ -39,5 +59,29 @@ namespace EAuction.Core.Tests
             //Assert
             Assert.Equal(800, auction.Winner.Amount);
         }
+
+        [Fact]
+        public void Auction_With_Many_Bids_And_Many_Interesteds()
+        {
+            //Arrange
+            var auction = new Auction("Van Gogh");
+            var johnDoe = new Interested("John Doe", auction);
+            var janeDoe = new Interested("Jane Doe", auction);
+            var johnSmith = new Interested("John Smith", auction);
+
+            auction.ReceiveBid(johnDoe, 800);
+            auction.ReceiveBid(janeDoe, 900);
+            auction.ReceiveBid(johnDoe, 1000);
+            auction.ReceiveBid(janeDoe, 990);
+            auction.ReceiveBid(johnSmith, 1400);
+
+            //Act
+            auction.ClosesAuction();
+
+            //Assert
+            Assert.Equal(1400, auction.Winner.Amount);
+            Assert.Equal(johnSmith, auction.Winner.Interested);
+        }
+
     }
 }
